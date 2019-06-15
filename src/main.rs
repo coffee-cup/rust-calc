6dacbdba57a762589a1fb0f2d17f64021bbcd53b
Jake Runzer <jakerunzer@gmail.com>
2019-06-15 03:51:57 +0100

    repl setup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
+use std::io;
+use std::io::Write;
+
 mod lexer;
 
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("Error flushing stdout");
+
+        match read_line().trim() {
+            "" => (),
+            line => {
+                let tokens = lexer::lex(&line.to_owned());
+                println!("{:?}", tokens)
+            }
+        }
+    }
+}
+
+fn read_line() -> String {
+    let mut buf = String::new();
+    match io::stdin().read_line(&mut buf) {
+        Ok(_) => buf,
+        Err(e) => {
+            println!("{:?}", e);
+            "".to_string()
+        }
+    }
 }
